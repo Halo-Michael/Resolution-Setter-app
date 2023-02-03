d6ca5999cc5bc2d5dd5cdc21944b79eb3cdc8054
Halo-Michael <yc992147156@me.com>
2023-02-04 00:05:16 +0800

    Update

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 xcodebuild clean build -quiet CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO PRODUCT_BUNDLE_IDENTIFIER="com.michael.resolutionsetter" -sdk iphoneos -configuration Release
+ldid -S -Icom.michael.resolutionsetter build/Release-iphoneos/resolutionsetter.app/resolutionsetter
 cd build/Release-iphoneos
 ln -s ./ Payload
 zip -r9 resolutionsetter.ipa Payload/resolutionsetter.app
